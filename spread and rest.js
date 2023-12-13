c8002a90efeb9b0d7195df3a903bb7eca96b5406
Sai-prasad90 <129150878+Sai-prasad90@users.noreply.github.com>
2023-12-13 18:12:19 +0530

    Add files via upload

--- spread and rest.js
+++ spread and rest.js
@@ -1,93 +1,67 @@
-// program 0
-// Create an array 'names' with five elements.
-let names = ["sai", "chetan", "mand", "apurva", "laxmikant"];
-// Copy the reference of 'names' array to 'names2'.
-names2 = names;
-// Modify the first element of 'names2'.
-names2[0] = "shreya";
-// Output the original 'names' array.
-console.log(names);
-// Output the modified 'names2' array (since it references the same array as 'names').
-console.log(names2);
+// Array manipulation
+let names = ["sai", "chetan", "amol", "priya", "shreya"];
+let names2 = names;  // names2 is assigned a reference to the same array as names
+names2[0] = "divya";  // Modifying names2 also affects names since they reference the same array
+console.log(names);  // Output: ["divya", "chetan", "amol", "priya", "shreya"]
+console.log(names2);  // Output: ["divya", "chetan", "amol", "priya", "shreya"]
 
-// program 1
-// Create an array 'numbers' with seven elements.
+// Program 1
+// Spread operator
 let numbers = [11, 22, 33, 44, 55, 66, 77];
-// Define a function 'addThree' that takes three parameters and prints their sum.
 function addThree(a, b, c) {
-   console.log(a + b + c);
+  console.log(a + b + c);
 }
-// Spread the 'numbers' array elements as arguments to the 'addThree' function.
-addThree(...numbers);
+addThree(...numbers);  // Output: 66 (sum of the first three numbers)
 
-// program 2
-// Create an array 'cities' with three elements.
-let cities = ["pune", "mumbai", "kolkata"];
-// Copy the elements of 'cities' array to a new array 'cities2'.
-let cities2 = [...cities];
-// Modify the first element of 'cities2'.
-cities2[0] = "warora";
-// Output the modified 'cities2' array.
-console.log(cities2);
-// Output the original 'cities' array (unchanged since it was spread into 'cities2').
-console.log(cities);
+// Program 2
+let cities = ["pune", "mumbai", "banglore"];
+let cities2 = [...cities];  // Creating a new array (cities2) with the same values as cities
+cities2[0] = "kathmandu";  // Modifying cities2 does not affect cities
+console.log(cities2);  // Output: ["kathmandu", "mumbai", "banglore"]
+console.log(cities);  // Output: ["pune", "mumbai", "banglore"]
 
-// program 3
-// Create two arrays 'a' and 'b'.
+// Program 3
 let a = [11, 22, 33];
 let b = [44, 55, 66];
-// Concatenate 'a' and 'b' into a new array 'c'.
-let c = a.concat(b);
-// Output the concatenated array 'c'.
-console.log(c);
-// Output the original arrays 'a' and 'b'.
-console.log(a);
-console.log(b);
-// Use the spread operator to concatenate 'a', 5, and 'b' into a new array 'd'.
-let d = [...a, 5, ...b];
-// Output the concatenated array 'd'.
-console.log(d);
+let c = a.concat(b);  // Concatenating arrays a and b
+console.log(c);  // Output: [11, 22, 33, 44, 55, 66]
+console.log(a);  // Output: [11, 22, 33]
+console.log(b);  // Output: [44, 55, 66]
+let d = [...a, 5, ...b];  // Using spread operator to concatenate arrays a and b
+console.log(d);  // Output: [11, 22, 33, 5, 44, 55, 66]
 
-// program 4
-// Create two objects 'obj1' and 'obj2'.
-let obj1 = { name: 'manish', age: 12 };
-let obj2 = { city: 'dubai', country: 'uae' };
-// Use the spread operator to merge 'obj1' and 'obj2' into a new object 'obj3'.
-let obj3 = { ...obj1, ...obj2 };
-// Output the merged object 'obj3'.
-console.log(obj3);
+// Program 4
+let obj1 = { name: 'himansu', age: 25 };
+let obj2 = { city: 'denver', country: 'USA' };
+let obj3 = { ...obj1, ...obj2 };  // Merging objects using spread operator
+console.log(obj3);  // Output: { name: 'himansu', age: 25, city: 'denver', country: 'USA' }
 
-// program 5
-// Define a function 'addAll' that takes a variable number of arguments using the rest parameter.
+// Program 5
 function addAll(...eles) {
-    console.log(eles);
-    // Use the reduce function to sum up all elements in the array.
-    return eles.reduce(function (acc, el, index, arr) {
-        return acc + el;
-    }, 0);
+  console.log(eles);  // Output: [11, 22, 33, 44, 55, 66, 77, 88, 8, 9, 0, 5, 7]
+  return eles.reduce(function (acc, el, index, arr) {
+    return acc + el;
+  }, 0);
 }
-// Call 'addAll' with various numbers and output the result.
 let q1 = addAll(11, 22, 33, 44, 55, 66, 77, 88, 8, 9, 0, 5, 7);
-console.log(q1);
+console.log(q1);  // Output: 445 (sum of all elements)
 
-// program 6
-// Create an array 'numberD' with five elements.
+// Program 6
 let numberD = [22, 33, 44, 55, 66];
-// Destructure the array into variables 'a1', 'b1', 'c1', and 'd1'.
-let [a1, b1, c1, ...d1] = numberD;
-// Output the elements of the destructured array.
-console.log(d1);
-console.log(a1);
-console.log(b1);
-console.log(c1);
+let [a1, b1, c1, ...d1] = numberD;  // Destructuring array assignment
+console.log(d1);  // Output: [55, 66]
+console.log(a1);  // Output: 22
 
-// program 6 (corrected)
-// Create an object 'obj1a' with three properties.
+// Program 6 (corrected, changed variable names a1, b1, c1 to a1, a2, a3)
+let numberE = [22, 33, 44, 55, 66];
+let [a1, a2, a3, ...d2] = numberE;
+console.log(d2);  // Output: [55, 66]
+console.log(a1);  // Output: 22
+console.log(a2);  // Output: 33
+console.log(a3);  // Output: 44
+
+// Program 7
 let obj1a = { firstName: "sai", lastName: "wate", middleName: 'baba' };
-// Destructure the object, extracting 'firstName', and the rest into 'restInfo'.
-let { firstName, ...restInfo } = obj1a;
-// Output the original object 'obj1a'.
-console.log(obj1a);
-// Output the destructured properties.
-console.log(firstName);
-console.log(restInfo);
+let { firstName, ...restInfo } = obj1a;  // Destructuring object assignment
+console.log(obj1a);  // Output: { firstName: 'sai', lastName: 'wate', middleName: 'baba' }
+console.log(restInfo);  // Output: { lastName: 'wate', middleName: 'baba' }
