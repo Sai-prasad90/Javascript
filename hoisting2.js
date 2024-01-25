4df7b11ee4817275a8592289eb3b238c54628dc9
Sai-prasad90 <129150878+Sai-prasad90@users.noreply.github.com>
2024-01-25 18:20:25 +0530

    Add files via upload

---
+++ hoisting2.js
@@ -0,0 +1,54 @@
+
+
+// greet()
+// function greet(){
+//     console.log("Happy happy")   // happy happy
+// }
+
+
+// console.log(a) //a not defined
+
+// console.log(a) 
+// var a //undeilned
+// var a =9  //undeined
+// console.log(a) //9
+
+
+// let a      //annot access 'a' before initialization
+//it will host but dead value
+
+//const a = 9; //Cannot access 'a' before initialization
+
+
+
+// greet()
+
+// const greet = () =>{
+//     console.log("function expression")
+// }  //Cannot access 'greet' before initialization
+
+
+// greet()
+//     var greet=()=>{
+//         console.log("var")
+
+//     } //greet is not a function
+     //function expression and class expression not hoisted
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
